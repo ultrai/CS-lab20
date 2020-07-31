5a20fd0fd5d2eeb133bc76975b324b58d7a7cff8
rahulgarhwal <68803781+rahulgarhwal@users.noreply.github.com>
2020-07-31 18:13:25 +0530

    Create 511756-exp13b2.m

---
+++ Exp13/511756-exp13b2.m
@@ -0,0 +1,9 @@
+%% effect of addition on closed loop zeroes
+sys=tf([1],[1 3 6]);
+z=[-1 -5 -10 -20];
+for i=1:4
+    sys_new=tf([1 -z(i), [1])*sys
+    subplot(2,2,i)
+    step(sys_new)
+    stepinfo(sys_new)
+end
