19913807666782f2136977838b17f05a99195368
jaswanth23 <68740635+jaswanth23@users.noreply.github.com>
2020-07-31 13:39:30 +0530

    511723 EXP12

---
+++ Exp12/511723/exp12bpole_code.m
@@ -0,0 +1,28 @@
+%% Time Response of Second order system
+z=[];
+p=[-10+30i -10-30i];
+k=1000;
+G=zpk(z,p,k)
+figure(3);
+step(G)
+%% Addition of Pole at -1 to the system
+z=[];
+p=[-10+30i -10-30i -1];
+k=1000;
+G=zpk(z,p,k)
+figure(4);
+step(G)
+%% Addition of Pole at -10 to the system
+z=[];
+p=[-10+30i -10-30i -10];
+k=1000;
+G=zpk(z,p,k)
+figure(5);
+step(G)
+%% Addition of Pole at -100 to the system
+z=[];
+p=[-10+30i -10-30i -100];
+k=1000;
+G=zpk(z,p,k)
+figure(6);
+step(G) 
