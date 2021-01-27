e4b2907224a29b07dea3af1a14bda9ca60c118b8
kum3445 <54733872+kum3445@users.noreply.github.com>
2021-01-27 20:42:36 +0530

    Add files via upload

---
+++ Exp1/511844_group10_exp1/511844_1b_expcode/Firstorder1b_zero_3.m
@@ -0,0 +1,7 @@
+%step response for First order system fo pole at s=-R,zero at s=-5R, R=44
+a=44;
+s=tf('s');
+G=(s/(5*a)+1)/(s+a);
+step(G)
+stepinfo(G)
+title("step response for First order system fo pole at s=-R,zero at s=-5R, R=44.")
