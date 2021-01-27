6f10720739f19f1c1457888f0c9f282df5022bd0
Vivek Kumar <77054201+Vikuzu@users.noreply.github.com>
2021-01-28 02:37:49 +0530

    Delete freq.m

--- Exp2/Batch_1/freq.m
+++
@@ -1,8 +0,0 @@
-%%code for Wr,Bandwidth,Mr
-w=input('Enter the natural frequency');
-e=input('Enter the damping ratio');
-num=[w*w];
-den=[1 2*e*w w*w];
-g=tf(num,den);
-[mr,wr]=getPeakGain(g);
-wb=bandwidth(g);
