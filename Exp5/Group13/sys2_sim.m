79dffe38abe102f04d594a15ac7360f719e7aef8
SwarazK <53287861+SwarazK@users.noreply.github.com>
2021-02-17 21:12:08 +0530

    Update sys2_sim.m

--- Exp5/Group13/sys2_sim.m
+++ Exp5/Group13/sys2_sim.m
@@ -12,36 +12,36 @@ p = [];
 kp = 1;
 fbp = zpk(z,p,kp);
 
-tf1 = feedback(plant1,fbp);
+tf1 = feedback(plant1,fbp,1);
 plot(step(tf1), 'DisplayName', 'Unity Positive Feedback');
 
 kn = -1;
 fbn = zpk(z,p,kn);
 
-tf1 = feedback(plant1,fbn);
+tf1 = feedback(plant1,fbn,1);
 plot(step(tf1), 'DisplayName', 'Unity Negative Feedback');
 
 p = [-1];
 fbp = zpk(z,p,kp);
 
-tf1 = feedback(plant1,fbp);
+tf1 = feedback(plant1,fbp,1);
 plot(step(tf1), 'DisplayName', 'Unity Positive Feedback pole at -1');
 
 fbn = zpk(z,p,kn);
 
-tf1 = feedback(plant1,fbn);
+tf1 = feedback(plant1,fbn,1);
 plot(step(tf1), 'DisplayName', 'Unity Negative Feedback pole at -1');
 
 p = [0];
 z = [-1]
 fbp = zpk(z,p,kp);
 
-tf1 = feedback(plant1,fbp);
+tf1 = feedback(plant1,fbp,1);
 plot(step(tf1), 'DisplayName', 'Unity Positive Feedback zero at -1');
 
 fbn = zpk(z,p,kn);
 
-tf1 = feedback(plant1,fbn);
+tf1 = feedback(plant1,fbn,1);
 plot(step(tf1), 'DisplayName', 'Unity Negative Feedback zero at -1');
 
 legend
