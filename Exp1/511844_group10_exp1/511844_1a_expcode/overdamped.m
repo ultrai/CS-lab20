be392ca192c9c5d211f0beedff6eb0a74f623f3a
kum3445 <54733872+kum3445@users.noreply.github.com>
2021-03-20 16:22:55 +0530

    Delete Exp1/511844_group10_exp1 directory

--- Exp1/511844_group10_exp1/511844_1a_expcode/overdamped.m
+++
@@ -1,7 +0,0 @@
-clc;
-clear;
-a=1/44;
-s=tf("s");
-G=1/(s*s+3*a*s+a*a);
-step(G)
-title('overdamped sysytem')
--- Exp1/511844_group10_exp1/511844_1a_expcode/undamped.m
+++
@@ -1,7 +0,0 @@
-clc;
-clear;
-a=1/44;
-s=tf("s");
-G=1/(s*s+2*a*a);
-step(G)
-title('undamped sysytem')
