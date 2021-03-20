35c62b18b5a09e397ba51faf35712c6f8c17fcfb
KIRAN SHEKOKAR <65600573+kiran284@users.noreply.github.com>
2021-03-20 23:28:28 +0530

    Delete Exp1/batch_13 directory

--- Exp1/batch_13/exp1_batch13_511833_code.m
+++
@@ -1,8 +0,0 @@
-%step response of a first order system
-%G(s)= 1/(s+3)
-a = [0,1];
-b = [1,3];
-sys = tf(a,b);
-t = feedback(sys,1)
-step(t);
-title('step response of first order system , G(s)=1/(s+3)')
