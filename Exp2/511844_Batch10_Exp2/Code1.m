4cd7a1b2c796118e26a0d27502dd4b6b54d071f9
kum3445 <54733872+kum3445@users.noreply.github.com>
2021-03-20 16:33:53 +0530

    Delete Exp2/511844_Batch10_Exp2 directory

--- Exp2/511844_Batch10_Exp2/Code1.m
+++
@@ -1,7 +0,0 @@
-clc; clear;
-s=tf('s')
-G=100/(s^2+10*s+100)
-[Mr,Wr]=getPeakGain(G)
-Wb=bandwidth(G)
-bode(G)
-margin(G)
--- Exp2/511844_Batch10_Exp2/Code3.m
+++
@@ -1,7 +0,0 @@
-clc; clear;
-s=tf('s')
-G=4/(s^2+4*s+4)
-[Mr,Wr]=getPeakGain(G)
-Wb=bandwidth(G)
-bode(G)
-margin(G)
