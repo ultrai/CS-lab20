a2bb88f880a9c322be00b3d4bb6a449b8e3eb65d
Karnatapu Sai Ashish <77603941+saiashishKarnatapu@users.noreply.github.com>
2021-03-20 18:44:18 +0530

    Add files via upload

---
+++ Exp5/BATCH_4_Exp_5/Exp_5_Batch_4_code_1.m
@@ -0,0 +1,27 @@
+s = tf('s')
+
+g_p = 1/((s+1)*(s+2))
+
+figure(1)
+step(g_p)
+stepinfo(g_p)
+
+figure(2)
+step(feedback(g_p,1))
+stepinfo(feedback(g_p,1))
+
+figure(3)
+step(feedback(g_p,-1))
+stepinfo(feedback(g_p,-1))
+
+figure(4)
+step(feedback(g_p,1/(s+1)))
+stepinfo(feedback(g_p,1/(s+1)))
+
+figure(5)
+step(feedback(g_p,(s+1)))
+stepinfo(feedback(g_p,(s+1)))
+
+figure(6)
+step(feedback(g_p,(s-1)))
+stepinfo(feedback(g_p,(s-1)))
