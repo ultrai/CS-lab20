52c91f39a18bdecb7037d9a7070e98c3ea634368
Karnatapu Sai Ashish <77603941+saiashishKarnatapu@users.noreply.github.com>
2021-04-05 17:26:41 +0530

    Delete Exp10/BATCH_4_Exp_10 directory

--- Exp10/BATCH_4_Exp_10/Exp_10_BATCH_4_code_3.m
+++
@@ -1,7 +0,0 @@
-num = input ('Enter numerator polynomial values in the form of matrix array');
-den1 = input ('Enter denominator 1 values');
-den2 = input ('Enter denominator 2 values');
-den = conv (den1,den2);
-H = tf (num,den);
-P = ss(H);
-[a,b,c,d] = ssdata(P);
