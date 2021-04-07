164a4954a22e7b19017c683384d31dc453275f7a
Vineetha7 <77395590+Vineetha7@users.noreply.github.com>
2021-04-07 15:04:23 +0530

    Exp10_batch11

---
+++ Exp10/Exp10_batch11/exp10_batch11_code_b2.m
@@ -0,0 +1,7 @@
+num = input ('Enter numerator polynomial values');
+den1 = input ('Enter Root 1 values');
+den2 = input ('Enter Root 2 values');
+s=tf('s')
+H = num/((s-den1)*(s-den2))
+P = ss(H)
+[a,b,c,d] = ssdata(P)
